6d234a1f88725298c7e939ce5c0c81719a67226d
giuseppevignanello <peppe.vignanello@gmail.com>
2023-10-26 16:55:34 +0200

    add migration and seeder

---
+++ database/migrations/2023_10_26_142504_create_jobs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('company');
+            $table->string('title');
+            $table->text('url');
+            $table->dateTime('time');
+            $table->text('description')->nullable();
+            $table->string('location')->nullable();
+            $table->boolean('sendOrSave')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
---
+++ database/seeders/JobSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Job;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class JobSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $jobs = json_decode(file_get_contents(__DIR__ . '/jobs.json'), true);
+
+        foreach ($jobs as $job) {
+            $newJob = new Job();
+            $newJob->company =  $job['company'];
+            $newJob->title = $job['title'];
+            $newJob->url = $job['url'];
+            $newJob->time = $job['time'];
+            $newJob->description = $job['description'];
+            $newJob->location = $job['location'];
+            $newJob->sendOrSave = $job['sendOrSave'];
+            $newJob->save();
+        }
+    }
+}
