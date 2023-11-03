4a0f60c37118ad4847ab38c268f69a3639d39ece
giuseppevignanello <peppe.vignanello@gmail.com>
2023-11-03 13:50:45 +0100

    add shadow

--- front-end/src/components/AppHeader.tsx
+++ front-end/src/components/AppHeader.tsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const AppHeader = () => {
-  return (
-    <div>
-        <header className='appHeader d-flex align-items-center justify-content-between'>
-          <div>
+    return (
+        <div>
+            <header className="appHeader d-flex align-items-center justify-content-between bg-light">
+                <div></div>
+                <div className="buttons me-2">
+                    <div>
+                        <button type="button" className="btn-primary btn">
+                            <Link
+                                className="text-white text-decoration-none"
+                                to={`/create`}
+                            >
+                                Add
+                            </Link>
+                        </button>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+};
 
-          </div>
-          <div className="buttons me-2">
-            <div>
-              <button type="button" className="btn-light btn">
-              <Link className='text-dark text-decoration-none' to={`/create`}>Add</Link>
-              </button>
-            </div>
-          </div>
-        </header>
-    </div>
-  )
-}
-
-export default AppHeader
+export default AppHeader;
